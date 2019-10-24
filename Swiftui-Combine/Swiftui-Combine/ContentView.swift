9827ea21f97f1d0599475b69e0b8cf31cd26c976
elmetal <nm7.pupu@gmail.com>
2019-10-24 13:49:10 +0900

    init project

---
+++ Swiftui-Combine/Swiftui-Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftui-Combine
+//  
+//  Created by elmetal on 2019/10/24
+//  Copyright © 2019 elmetal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
